f7c65e77b1c57b02ed48cf5a79194fd5b4fbc7b3
Guto Xavier <gutoffline@gmail.com>
2019-09-27 22:33:03 -0300

    teste

---
+++ TransporteDeEscravos/Assets/Scripts/Jogo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Jogo : MonoBehaviour
+{
+
+    public void CarregaIntroducao()
+    {
+        SceneManager.LoadScene("Introducao");
+    }
+
+    public void EncerrarJogo()
+    {
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
